09bc133e245f0a41ce919fa350be29b7b32bc052
陳昱睿 <chenyurui@chenyuruideMBP.mshome.net>
2025-05-22 22:23:42 +0800

    reconstruct

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 # Fastapi進入點
 from fastapi import FastAPI
-from auth import router as auth_router
+from api.auth import router as auth_router
 from models import Base
 from core.db import engine
-from api.main import router as api_router
+from api.user import router as api_router
 
 app = FastAPI()
 app.include_router(auth_router)
