30e83e7ffafb68e66056d9ac891ef85fec3647ec
xrueiii <b11705043@g.ntu.edu.tw>
2025-05-25 20:26:02 +0800

    finish others api (no AI Model yet)

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ from fastapi.openapi.utils import get_openapi
 from api.auth import router as auth_router
 from api.user import router as api_router
 from api.task import router as task_router
+from api.file import router as file_router 
+from api.assistant import router as assistant_router 
 from models import Base
 from core.db import engine
 
@@ -14,6 +16,8 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(api_router)
 app.include_router(task_router)
+app.include_router(file_router) 
+app.include_router(assistant_router)
 
 # 建立資料表
 Base.metadata.create_all(bind=engine)
