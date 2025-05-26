e661f1c22dd5b22b4afd657b080e6bd24e67aa6b
陳昱睿 <chenyurui@chenyuruideMacBook-Pro.local>
2025-05-26 16:15:27 +0800

    user

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 from crud.crud_user import get_current_user
 
-router = APIRouter(tags=["Users"])
+router = APIRouter(tags=["User"])
 
-@router.get("/users/profile")
+@router.get("/user/profile")
 def get_user_profile(current_user = Depends(get_current_user)):
     return {
         "user_id": f"u{current_user.id}",
