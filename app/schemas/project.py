5ff40572ce5a3a44f2bbeb0fbaab08dc4b382ece
howard921109 <howard921109@gmail.com>
2025-06-02 01:45:35 +0800

    feat: use gemini to reschedule when there is a new task or a task is updated

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -12,6 +12,7 @@ class ProjectSchema(BaseModel):
     current_milestone: str
 
     class Config:
+        from_attributes = True
         orm_mode = True
 
 
@@ -45,6 +46,7 @@ class TaskSchema(BaseModel):
     isCompleted: bool
 
     class Config:
+        from_attributes = True
         orm_mode = True
 
 
